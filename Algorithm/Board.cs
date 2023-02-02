6bbd071055129205c7d9cdee7dd30a063a427cab
OhJaeYoung <ostyoung@naver.com>
2023-02-02 23:35:39 +0900

    Linked List

--- Algorithm/Board.cs
+++ Algorithm/Board.cs
@@ -4,6 +4,7 @@ using System.Text;
 
 namespace Algorithm
 {
+
     class MyList<T>
     {
         const int DEFAULT_SIZE = 1;
@@ -16,7 +17,7 @@ namespace Algorithm
         public void Add(T item)         // List의 Add함수 구현
         {
             // 1. 공간이 충분히 남아있는지 확인한다
-            if(Count >= Capacity)
+            if (Count >= Capacity)
             {
                 // 공간을 다시 늘려서 확보한다
                 T[] newArray = new T[Count * 2];
@@ -28,6 +29,7 @@ namespace Algorithm
             _data[Count] = item;
             Count++;
         }
+
 
         // O(1)
         public T this[int index]        // 인덱스 설정
@@ -46,13 +48,68 @@ namespace Algorithm
         }
     }
 
+    class MyLinkedListNode<T>      // LinkedList Node 구현
+    {
+        public T Data;
+        public MyLinkedListNode<T> Next;
+        public MyLinkedListNode<T> Prev;
+    }
+
+    class MyLinkedList<T>   // LinkedList 구현
+    {
+        public MyLinkedListNode<T> Head = null;    // 첫번째
+        public MyLinkedListNode<T> Tail = null;    // 마지막
+        public int Count = 0;
+
+        // O(1)
+        public MyLinkedListNode<T> AddLast(T data)
+        {
+            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
+            newRoom.Data = data;
+
+            // 만약에 아직 방이 아예 없었다면, 새로 추가한 첫번째 방이 곧 Head이다.
+            if (Head == null)
+                Head = newRoom;
+
+            // 기존의 [마지막 방]과 [새로 추가되는 방]을 연결해준다.
+            if(Tail != null)
+            {
+                Tail.Next = newRoom;
+                newRoom.Prev = Tail;
+            }
+
+            // [새로 추가되는 방]을 [마지막 방]으로 인정한다. 
+            Tail = newRoom;
+            Count++;
+            return newRoom;
+        }
+
+        // O(1)
+        public void Remove(MyLinkedListNode<T> room)
+        {
+            // [기존의 첫번째 방의 다음 방]을 [첫번째 방]으로 인정한다.
+            if (Head == room)
+                Head = Head.Next;
+
+            // [기존의 마지막 방의 이전 방]을 [마지막 방]으로 인정한다.
+            if (Tail == room)
+                Tail = Tail.Prev;
+            if(room.Prev != null)
+                room.Prev.Next = room.Next;
+            if(room.Next != null)
+                room.Next.Prev = room.Prev;
+
+            Count--;
+        }
+    }
+
     class Board
     {
         public int[] _data = new int[25];   // 배열
         public MyList<int> _data2 = new MyList<int>();  // 동적 배열
-        public LinkedList<int> _data3 = new LinkedList<int>();  //연결 리스트
+        public MyLinkedList<int> _data3 = new MyLinkedList<int>();  //연결 리스트
 
-        public void Initialize()
+        public void InitializeMyList()
         {
             _data2.Add(101);
             _data2.Add(102);
@@ -63,6 +120,17 @@ namespace Algorithm
             int temp = _data[2];
 
             _data2.RemoveAt(2);
+        }
+
+        public void InitializeRoom()
+        {
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            MyLinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
+
+            _data3.Remove(node);
         }
     }
 }
